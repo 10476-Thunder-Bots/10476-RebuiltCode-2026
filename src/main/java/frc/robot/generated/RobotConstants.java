b6b00ab16aac3ecb3423bd6efaab9715aa7a89b8
gamersguild09 <jaxsonhanks9@icloud.com>
2026-01-27 17:18:30 -0600

    renamed limilights
    renamed fr this time

--- src/main/java/frc/robot/generated/RobotConstants.java
+++ src/main/java/frc/robot/generated/RobotConstants.java
@@ -18,8 +18,8 @@ public final class RobotConstants {
         public static final PIDConstants ROTATION_PID = new PIDConstants(0, 0, 0);
     }
         public final class LimeLight{
-        public static final String LEFT_LIMELIGHT_NAME = "uncleft";
-        public static final String RIGHT_LIMELIGHT_NAME = "uncleright";
+        public static final String LEFT_LIMELIGHT_NAME = "limelight-uncleft";
+        public static final String RIGHT_LIMELIGHT_NAME = "limelight-uncleright";
         private static final double XY_STD_DEV = 0.2;
         private static final double THEATA_STD_DEV = 1.5;
         public static final Matrix<N3,N1> STD_DEVS = VecBuilder.fill(XY_STD_DEV,XY_STD_DEV,THEATA_STD_DEV);
