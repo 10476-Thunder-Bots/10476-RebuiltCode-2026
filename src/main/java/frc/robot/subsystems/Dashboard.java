3e2daf3354b3c7556b94435524786df7f63e2c32
Malachi McArthur <malachi0mcarthur@gmail.com>
2026-01-27 15:41:58 -0600

    Init commit

---
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generated.RobotConstants;
+
+public class Dashboard extends SubsystemBase { 
+    private final SendableChooser<String> autoStartPositionChooser;
+    private PathPlannerAuto selectAuto;
+
+    public Dashboard(){
+        autoStartPositionChooser = new SendableChooser<String>();
+        autoStartPositionChooser.addOption("Left", "Left");
+        autoStartPositionChooser.addOption("Middle", "Middle");
+        autoStartPositionChooser.addOption("Right", "Right");
+        SmartDashboard.putData("StartPosition", autoStartPositionChooser);
+    }
+
+
+    @Override
+    public void periodic() {
+     switch (autoStartPositionChooser.getSelected()) {
+        case "Left":
+            selectAuto = RobotConstants.Autos.Left;
+            break;
+        case "Middle":
+            selectAuto = RobotConstants.Autos.Middle;
+            break;
+        case "Right":
+            selectAuto = RobotConstants.Autos.Right;
+            break;
+        default:
+
+            break;
+     }  
+
+     if (selectAuto != null){
+
+     }
+
+        
+    }
+    public Command getSelectAuto(){
+        return selectAuto;
+    }
+}
