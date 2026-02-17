bad94b910fba931912b39b46ab233d4d53b6bdd4
Malachi McArthur <malachi0mcarthur@gmail.com>
2026-02-17 16:59:27 -0600

    reformated turret to use 2 subclasses

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -1,24 +1,13 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Degree;
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Radians;
-
-import com.pathplanner.lib.commands.PathPlannerAuto;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.generated.RobotConstants;
 
 public class Dashboard extends SubsystemBase {
     private CommandSwerveDrivetrain drivetrain;
-    private Turret turret;
 
     public Dashboard(CommandSwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -4,102 +4,113 @@ import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Pounds;
-import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.Seconds;
 
-import com.ctre.phoenix6.Utils;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.hardware.TalonFXS;
-import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
-import com.fasterxml.jackson.databind.node.ShortNode;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.AnalogEncoder;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Telemetry;
 import frc.robot.generated.RobotConstants;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
-import yams.mechanisms.config.FlyWheelConfig;
 import yams.mechanisms.config.PivotConfig;
 import yams.mechanisms.positional.Pivot;
-import yams.mechanisms.velocity.FlyWheel;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
-import yams.motorcontrollers.remote.TalonFXSWrapper;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class Turret extends SubsystemBase {
-    private PIDController turretController;
-    private AnalogEncoder analogEncoder;
-    private TalonFX turretMotor;
-    private SmartMotorControllerConfig motorConfig;
-    private SmartMotorController motor;
-    private PivotConfig pConfig;
-    private Pivot pivot;
+    private Swivel swivel;
     private CommandSwerveDrivetrain drivetrain;
     private Dashboard dashboard;
 
     public Turret(CommandSwerveDrivetrain drivetrain, Dashboard dashboard) {
+        swivel = new Swivel(this);
         this.dashboard = dashboard;
         this.drivetrain = drivetrain;
-        turretController = new PIDController(RobotConstants.Turret.TURRET_KP, RobotConstants.Turret.TURRET_KI,
-                RobotConstants.Turret.TURRET_KD);
-        turretController.disableContinuousInput();
-        analogEncoder = new AnalogEncoder(RobotConstants.Turret.ENCODER_ID, 10.0, 0);
-        turretMotor = new TalonFX(RobotConstants.Turret.TURRET_CAN_ID);
-
-        motorConfig = new SmartMotorControllerConfig(this)
-                .withControlMode(ControlMode.CLOSED_LOOP)
-                .withClosedLoopController(RobotConstants.Turret.TURRET_KP, RobotConstants.Turret.TURRET_KI,
-                        RobotConstants.Turret.TURRET_KD, RobotConstants.Turret.TURRET_MAX_VEL,
-                        RobotConstants.Turret.TURRET_MAX_ACC)
-                .withGearing(new MechanismGearing(GearBox.fromReductionStages(9)))
-                .withIdleMode(MotorMode.BRAKE)
-                .withMotorInverted(false)
-                .withTelemetry("TurretMotor", TelemetryVerbosity.HIGH)
-                .withStatorCurrentLimit(Amps.of(40))
-                .withClosedLoopRampRate(Seconds.of((.25)))
-                .withOpenLoopRampRate(Seconds.of((.25)));
-        // .withExternalEncoder(analogEncoder);
-        motor = new TalonFXWrapper(turretMotor, DCMotor.getKrakenX60(1), motorConfig);
-
-        pConfig = new PivotConfig(motor)
-                .withStartingPosition(Degrees.of(0))
-                .withTelemetry("TurretPivot", TelemetryVerbosity.HIGH)
-                .withHardLimit(Degrees.of(-180), Degrees.of(180))
-                .withMOI(Inches.of(4), Pounds.of(2.72));
-        pivot = new Pivot(pConfig);
     }
 
     @Override
     public void periodic() {
-        pivot.updateTelemetry();
-        SmartDashboard.putNumber("Angle from pivot", pivot.getAngle().in(Degrees));
-        pivot.setMechanismPositionSetpoint(getTurretSetpoint());
+        swivel.updateTelemetry();
+        swivel.runSetPoint();
     }
 
     @Override
     public void simulationPeriodic() {
-        pivot.simIterate();
+        swivel.simIterate();
     }
 
-    public Angle getTurretSetpoint() {
-        Rotation2d aimAngle = dashboard.shootAngle();
-        return aimAngle.minus(drivetrain.getPigeon2().getRotation2d()).getMeasure();
+    private class Swivel {
+        private PIDController turretController;
+        private TalonFX turretMotor;
+        private SmartMotorControllerConfig motorConfig;
+        private SmartMotorController motor;
+        private PivotConfig pConfig;
+        private Pivot pivot;
+
+        private Swivel(Turret turret) {
+            turretController = new PIDController(RobotConstants.Turret.TURRET_KP, RobotConstants.Turret.TURRET_KI,
+                    RobotConstants.Turret.TURRET_KD);
+            turretController.disableContinuousInput();
+            turretMotor = new TalonFX(RobotConstants.Turret.TURRET_CAN_ID);
+            motorConfig = new SmartMotorControllerConfig(turret)
+                    .withControlMode(ControlMode.CLOSED_LOOP)
+                    .withClosedLoopController(RobotConstants.Turret.TURRET_KP, RobotConstants.Turret.TURRET_KI,
+                            RobotConstants.Turret.TURRET_KD, RobotConstants.Turret.TURRET_MAX_VEL,
+                            RobotConstants.Turret.TURRET_MAX_ACC)
+                    .withGearing(new MechanismGearing(GearBox.fromReductionStages(9)))
+                    .withIdleMode(MotorMode.BRAKE)
+                    .withMotorInverted(false)
+                    .withTelemetry("TurretMotor", TelemetryVerbosity.HIGH)
+                    .withStatorCurrentLimit(Amps.of(40))
+                    .withClosedLoopRampRate(Seconds.of((.25)))
+                    .withOpenLoopRampRate(Seconds.of((.25)));
+            // .withExternalEncoder(new AnalogEncoder(RobotConstants.Turret.ENCODER_ID,
+            // 10.0, 0));
+            motor = new TalonFXWrapper(turretMotor, DCMotor.getKrakenX60(1), motorConfig);
+            pConfig = new PivotConfig(motor)
+                    .withStartingPosition(Degrees.of(0))
+                    .withTelemetry("TurretPivot", TelemetryVerbosity.HIGH)
+                    .withHardLimit(Degrees.of(-180), Degrees.of(180))
+                    .withMOI(Inches.of(4), Pounds.of(2.72));
+            pivot = new Pivot(pConfig);
+        }
+
+        private Angle getTurretSetpoint() {
+            Rotation2d aimAngle = dashboard.shootAngle();
+            return aimAngle.minus(drivetrain.getPigeon2().getRotation2d()).getMeasure();
+        }
+
+        public void updateTelemetry() {
+            pivot.updateTelemetry();
+        }
+
+        public void simIterate() {
+            pivot.simIterate();
+        }
+
+        public void runSetPoint() {
+            pivot.setMechanismPositionSetpoint(getTurretSetpoint());
+        }
+
+        private Boolean targeted() {
+            return true;// TODO: add check to see if pivot is at the setpoint
+        }
     }
 
+    private class Shooter {
+        public Shooter() {
+        }
+
+        private Boolean shooterReady() {
+            return true;// TODO: Add check to see if shooter meets conditions to fire
+        }
+    }
 }
