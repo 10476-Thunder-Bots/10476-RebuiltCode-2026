89da205548a0b105b50ee779fe57dd0eedad524c
Malachi McArthur <malachi0mcarthur@gmail.com>
2026-03-22 20:18:50 -0500

    changed shake bot and edited button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,15 +10,12 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Dashboard;
@@ -106,16 +103,6 @@ public class RobotContainer {
                 swivel.setDefaultCommand(swivel.setdutyCycle(0));
                 loader.setDefaultCommand(loader.run(() -> loader.setLoader(0)));
                 joystick.button(7).whileTrue(drivetrain.applyRequest(() -> brake));
-                joystick.button(1).whileTrue(drivetrain
-                                .applyRequest(() -> point.withModuleDirection(
-                                                new Rotation2d(-joystick.getY(), -joystick.getX()))));
-
-                // Run SysId routines when holding back/start and X/Y.
-                // Note that each routine should be run exactly once in a single log.
-                joystick.button(1).and(joystick.trigger()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-                joystick.button(1).and(joystick.trigger()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-                joystick.button(1).and(joystick.trigger()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-                joystick.button(1).and(joystick.trigger()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
                 // reset the field-centric heading on button 6 press
                 joystick.button(6).onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
@@ -127,19 +114,14 @@ public class RobotContainer {
                                 .whileTrue(shooter.run(() -> shooter.setVelocity(
                                                 MetersPerSecond.of(dashboard.manuelShootSpeed()))));
 
-                joystick.button(4).whileTrue(CompositeCommands.runLoader());
-
                 joystick.button(4).whileTrue(CompositeCommands.shakeBot());
                 joystick.button(3).toggleOnTrue(drivetrain.runOnce(() -> autoCommands.choosePath()));
                 joystick.button(2).toggleOnTrue(drivetrain.runOnce(() -> autoCommands.cancelPaths()));
-                xboxjoystick.button(3).whileTrue(loader.run
-                (() -> loader.setLoader(.3)));
-                xboxjoystick.button(4).whileTrue(CompositeCommands.swivelShoot());
+                xboxjoystick.button(3).whileTrue(CompositeCommands.runLoader());
+                xboxjoystick.button(4).toggleOnTrue(CompositeCommands.swivelShoot());
                 xboxjoystick.button(2).toggleOnTrue(CompositeCommands.intakeOn())
-                .onFalse(CompositeCommands.intakeOff());
+                                .onFalse(CompositeCommands.intakeOff());
                 drivetrain.registerTelemetry(logger::telemeterize);
-                
-
 
         }
 
